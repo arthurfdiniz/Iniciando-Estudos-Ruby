0150ce5ad3490b40e8109b3fe2f7f54fd4978b90
arthurfdiniz <artl-lur@hotmail.com>
2023-11-20 14:13:55 -0300

    first commit

--- calc.rb
+++ calc.rb
@@ -1,10 +1,15 @@
-puts "Olá! Vamos somar alguns números?"
+puts "Olá! Vamos somar subitrair e dividir a alguns números?"
 puts "Escolha um número de 0 a 100: "
 number1 = gets.chomp.to_i
-puts "Legal... agora digite o próximo número que queira somar: "
+puts "Legal... agora digite o próximo número: "
 number2 = gets.chomp.to_i
 puts "Legal... aguarde um instante!"
 sleep(3)
 puts "..."
 puts "O resultado da soma do primeiro número (#{number1}) com o segundo número (#{number2}) é: #{number1 + number2} !"
-sleep(3)
+sleep(2)
+puts "O resultado da multiplicação do primeiro número (#{number1}) com o segundo número (#{number2}) é: #{number1 * number2} !"
+sleep(2)
+puts "O resultado da divisão do primeiro número (#{number1}) com o segundo número (#{number2}) é: #{number1 / number2} !"
+sleep(4)
+puts "O resultado da subitração do primeiro número (#{number1}) com o segundo número (#{number2}) é: #{number1 - number2} !"
